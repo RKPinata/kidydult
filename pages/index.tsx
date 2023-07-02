c5dd9fa417fd412ff4afd84ea8e3b0c6b2a4cfa2
Danish Teuku <teukutiga@gmail.com>
2023-07-02 14:34:38 +0800

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,174 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { ChangeEvent, MouseEvent, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+import uploadFiles  from './api/upload';
 
-export default function Home() {
+
+const Home: NextPage = () => {
+  const [files, setFiles] = useState<File[]>([]);
+  const [previewUrls, setPreviewUrls] = useState<string[]>([]);
+
+  const handleFile = (fileList: FileList | null) => {
+    if (!fileList || fileList.length === 0) {
+      alert("No file was chosen");
+      return;
+    }
+
+    const uploadedFiles = Array.from(fileList);
+
+    const invalidFiles = uploadedFiles.filter(
+      (file) => !file.type.startsWith("text/plain")
+    );
+    if (invalidFiles.length > 0) {
+      alert("Please select valid .txt files.");
+      return;
+    }
+
+    setFiles((prevFiles) => [...prevFiles, ...uploadedFiles]);
+    setPreviewUrls((prevUrls) => [
+      ...prevUrls,
+      ...uploadedFiles.map((file) => file.name),
+    ]);
+  };
+
+  const onFileUploadChange = (event: ChangeEvent<HTMLInputElement>) => {
+    handleFile(event.target.files);
+  };
+
+  const dropFileHandler = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    handleFile(event.dataTransfer.files);
+    console.log("success");
+  };
+
+  const preventDefaultHandler = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  const onCancelFileHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (!files.length) {
+      return;
+    }
+    setFiles([]);
+    setPreviewUrls([]);
+  };
+
+  const onUploadFileHandler = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  
+    //send file to backend
+    // Send files to the backend
+  const formData = new FormData();
+  files.forEach((file) => {
+    formData.append('files', file);
+  });
+
+  try {
+    const response = await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      // Files uploaded successfully
+      console.log('Files uploaded successfully.');
+      
+    } else {
+      // Handle the error case
+      console.error('Failed to upload files.');
+    }
+  } catch (error) {
+    // Handle any network or other errors
+    console.error('An error occurred while uploading files:', error);
+  }
+
+  };
+  
+  
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Head>
+        <title>File uploader</title>
+        <meta name="description" content="File uploader" />
+      </Head>
+
+      <main className="py-10">
+        <div className="w-full min-h-screen max-w-3xl px-3 mx-auto">
+          <h1 className="mb-10 text-3xl font-bold text-gray-900">
+            Upload your files {'(.txt)'}
+          </h1>
+
+          <form onSubmit={(event) => event.preventDefault()}>
+            <div
+              className="flex p-3 border border-gray-500 border-dashed gap-1.5"
+              onDrop={dropFileHandler}
+              onDragOver={preventDefaultHandler}
+            >
+              <label className="flex flex-col items-center justify-center flex-grow h-full py-3 transition-colors duration-150 cursor-pointer hover:text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-14 h-14"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
+                  />
+                </svg>
+                <strong className="text-sm font-medium">
+                  Drag and drop your file here
+                </strong>
+                <input
+                  className="block w-0 h-0"
+                  name="file"
+                  type="file"
+                  onChange={onFileUploadChange}
+                  multiple
+                />
+              </label>
+            </div>
+            {previewUrls.length > 0 && (
+              <div>
+                <h2>You are uploading:</h2>
+                <ul>
+                  {previewUrls.map((url, index) => (
+                    <li key={index}>{url}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
+            <div className="flex mt-4 gap-1.5">
+              <button
+                onClick={onUploadFileHandler}
+                disabled={files.length === 0}
+                className="w-1/2 px-4 py-3 text-sm font-medium text-white transition-colors duration-300 bg-gray-700 rounded-sm md:w-auto md:text-base disabled:bg-gray-400 hover:bg-gray-600"
+              >
+                Upload files
+              </button>
+              <button
+                onClick={onCancelFileHandler}
+                disabled={files.length === 0}
+                className="w-1/2 px-4 py-3 text-sm font-medium text-white transition-colors duration-300 bg-gray-700 rounded-sm md:w-auto md:text-base disabled:bg-gray-400 hover:bg-gray-600"
+              >
+                Remove files
+              </button>
+            </div>
+          </form>
         </div>
-      </div>
+      </main>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
